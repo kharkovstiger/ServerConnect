89ffd5e52d79a467c9fa9cc943d1d608a3c29cfc
Tiger <kharkovs.tiger@gmail.com>
2016-12-21 20:39:02 +0200

    update

---
+++ app/src/main/java/com/example/tiger/serverconnect/tasks/AuthTask.java
@@ -0,0 +1,33 @@
+package com.example.tiger.serverconnect.tasks;
+
+import android.os.AsyncTask;
+
+import com.example.tiger.serverconnect.providers.Response;
+
+public class AuthTask extends AsyncTask<Void, Void, Response>{
+    private AuthListener listener;
+    private String email;
+    private String password;
+
+    public AuthTask(AuthListener listener, String email, String password) {
+        this.listener = listener;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    protected Response doInBackground(Void... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Response response) {
+        super.onPostExecute(response);
+        if (listener!=null)
+            ;
+    }
+
+    public interface AuthListener {
+        void authCallBack(Response response);
+    }
+}
